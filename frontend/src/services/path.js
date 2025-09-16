84b4775896d63ad81a9247f0485dd66e8ee3cdb8
s-evg <s-evg@bk.ru>
2025-09-16 13:18:55 +0500

    /api

--- frontend/src/services/path.js
+++ frontend/src/services/path.js
@@ -1,4 +1,5 @@
 //const API_BASE_URL = "http://localhost:8000/api"; // локально
-const API_BASE_URL = 'http://176.108.254.47:8000/api'; // на сервере
+// const API_BASE_URL = 'http://176.108.254.47:8000/api'; // на сервере
+const API_BASE_URL = '/api'; // на сервере
 
 export default API_BASE_URL;
